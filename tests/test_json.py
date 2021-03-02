56224ce7fde9e5184e0b44f8c0fe25ceb5829420
bugaga427 <vladislav3372@gmail.com>
2021-03-02 18:15:25 +0300

    fix test_json.py

--- tests/test_json.py
+++ tests/test_json.py
@@ -5,5 +5,5 @@ from gendiff.engine import generate_diff
 def test_json():
     before = json.load(open('tests/fixtures/json_before.json'))
     after = json.load(open('tests/fixtures/json_after.json'))
-    result = open('tests/fixtures/json_diff.txt').read()
-    assert generate_diff(before, after) == result
+    result = open('tests/fixtures/json_diff.txt')
+    assert generate_diff(before, after) == result.read()
