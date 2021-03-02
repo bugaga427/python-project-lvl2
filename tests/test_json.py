7b7134c0da2691e7216eb361b4762926302df719
bugaga427 <vladislav3372@gmail.com>
2021-03-02 03:37:23 +0300

    add test

---
+++ tests/test_json.py
@@ -0,0 +1,9 @@
+import json
+from gendiff.engine import generate_diff
+
+
+def test_json():
+    before = json.load(open('tests/fixtures/json_before.json'))
+    after = json.load(open('tests/fixtures/json_after.json'))
+    result = open('tests/fixtures/json_diff.txt').read()
+    assert generate_diff(before, after) == result
