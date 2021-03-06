42df60835f6e0b54046e213b4c59835399ee5221
bugaga427 <vladislav3372@gmail.com>
2021-03-06 21:57:03 +0300

    fix engine.py

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,27 +1,27 @@
 def generate_diff(file_before, file_after):
-    result = '{\n'
+    result = "{\n"
     keys = sorted(set(file_before) | set(file_after))
     for key in keys:
         if key in file_before:
             if key in file_after:
                 if file_before[key] == file_after[key]:
-                    result += f'    {key}: {file_before[key]}\n'
+                    result += f"    {key}: {file_before[key]}\n"
                 else:
-                    result += f'  - {key}: {file_before[key]}\n'
-                    result += f'  + {key}: {file_after[key]}\n'
+                    result += f"  - {key}: {file_before[key]}\n"
+                    result += f"  + {key}: {file_after[key]}\n"
             else:
-                result += f'  - {key}: {file_before[key]}\n'
+                result += f"  - {key}: {file_before[key]}\n"
         else:
-            result += f'  + {key}: {file_after[key]}\n'
-    result += '}'
+            result += f"  + {key}: {file_after[key]}\n"
+    result += "}"
     return convert_to_json_style(result)
 
 
 def convert_to_json_style(message):
-    if 'False' in message:
-        message = message.replace('False', 'false')
-    if 'True' in message:
-        message = message.replace('True', 'true')
-    if 'None' in message:
-        message = message.replace('None', 'null')
+    if "False" in message:
+        message = message.replace("False", "false")
+    if "True" in message:
+        message = message.replace("True", "true")
+    if "None" in message:
+        message = message.replace("None", "null")
     return message
