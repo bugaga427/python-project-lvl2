5ac81d586341d1729abc6b79add32906bc599e62
bugaga427 <vladislav3372@gmail.com>
2021-03-20 01:29:16 +0300

    added module with data parsing

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,6 +1,4 @@
 import argparse
-import json
-import yaml
 
 
 def generate_parser():
@@ -10,23 +8,3 @@ def generate_parser():
     parser.add_argument("second_file")
     args = parser.parse_args()
     return args
-
-
-def get_arguments():
-    if generate_parser().first_file.endswith('.json') and \
-       generate_parser().second_file.endswith('.json'):
-        arg1 = json.load(open(generate_parser().first_file))
-        arg2 = json.load(open(generate_parser().second_file))
-
-    elif generate_parser().first_file.endswith(('.yaml', '.yml')) and \
-            generate_parser().second_file.endswith(('.yaml', '.yml')):
-        arg1 = yaml.load(
-            open(generate_parser().first_file),
-            Loader=yaml.FullLoader
-        )
-        arg2 = yaml.load(
-            open(generate_parser().second_file),
-            Loader=yaml.FullLoader
-        )
-
-    return arg1, arg2
---
+++ gendiff/parsing.py
@@ -0,0 +1,29 @@
+from gendiff.parser import generate_parser
+import json
+import yaml
+
+
+def parsing_args():
+    if is_json(generate_parser().first_file, generate_parser().second_file):
+        arg1 = json.load(open(generate_parser().first_file))
+        arg2 = json.load(open(generate_parser().second_file))
+
+    elif is_yaml(generate_parser().first_file, generate_parser().second_file):
+        arg1 = yaml.load(
+            open(generate_parser().first_file),
+            Loader=yaml.FullLoader
+        )
+        arg2 = yaml.load(
+            open(generate_parser().second_file),
+            Loader=yaml.FullLoader
+        )
+
+    return arg1, arg2
+
+
+def is_json(file1, file2):
+    return file1.endswith(".json") and file2.endswith(".json")
+
+
+def is_yaml(file1, file2):
+    return file1.endswith((".yaml", "yml")) and file2.endswith((".yaml", "yml"))
