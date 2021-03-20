0eff19e2053fcb195f72d31160194a71692639f4
bugaga427 <vladislav3372@gmail.com>
2021-03-20 15:47:21 +0300

    fix test_yaml.py

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -3,7 +3,13 @@ import yaml
 
 
 def test_yaml():
-    before = yaml.load(open('tests/fixtures/yaml_before.yaml'))
-    after = yaml.load(open('tests/fixtures/yaml_after.yaml'))
+    before = yaml.load(
+        open('tests/fixtures/yaml_before.yaml'),
+        Loader=yaml.FullLoader
+    )
+    after = yaml.load(
+        open('tests/fixtures/yaml_after.yaml'),
+        Loader=yaml.FullLoader
+    )
     result = open("tests/fixtures/diff.txt")
     assert generate_diff(before, after) == result.read()
