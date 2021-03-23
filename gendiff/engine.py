a13f5e277e8002670ea0f7ce4035bcc07113a1f9
bugaga427 <vladislav3372@gmail.com>
2021-03-23 12:52:22 +0300

    update engine.py

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,22 +1,109 @@
-def generate_diff(file_before, file_after):
+def generate_diff(file_before, file_after, nesting_lvl=0):
     result = "{\n"
     keys = sorted(set(file_before) | set(file_after))
     for key in keys:
         if key in file_before and key not in file_after:
-            result += f"  - {key}: {file_before[key]}\n"
+            if isinstance(file_before.get(key), dict):
+                result += "    " * nesting_lvl + key_deleted(
+                    key, generate_diff(
+                        file_before[key],
+                        file_before[key],
+                        nesting_lvl=nesting_lvl + 1
+                    )
+                )
+            else:
+                result += "    " * nesting_lvl + key_deleted(
+                    key, file_before[key]
+                )
         elif key in file_after and key not in file_before:
-            result += f"  + {key}: {file_after[key]}\n"
+            if isinstance(file_after.get(key), dict):
+                result += "    " * nesting_lvl + key_added(
+                    key, generate_diff(
+                        file_after[key],
+                        file_after[key],
+                        nesting_lvl=nesting_lvl + 1
+                    )
+                )
+            else:
+                result += "    " * nesting_lvl + key_added(key, file_after[key])
         else:
-            result += check_values(key, file_before, file_after)
-    result += "}"
+            if file_before[key] == file_after[key]:
+                if isinstance(file_before.get(key), dict):
+                    result += "    " * nesting_lvl + key_not_changed(
+                        key, generate_diff(
+                            file_before[key],
+                            file_after[key],
+                            nesting_lvl=nesting_lvl + 1
+                        )
+                    )
+                else:
+                    result += "    " * nesting_lvl + key_not_changed(
+                        key, file_before[key]
+                    )
+            else:
+                if isinstance(
+                    file_before.get(key), dict
+                ) and isinstance(
+                    file_after[key], dict
+                ):
+                    result += "    " * nesting_lvl + key_not_changed(
+                        key, generate_diff(
+                            file_before[key],
+                            file_after[key],
+                            nesting_lvl=nesting_lvl + 1
+                        )
+                    )
+                elif isinstance(
+                    file_before.get(key), dict
+                ) and not isinstance(
+                    file_after[key], dict
+                ):
+                    result += "    " * nesting_lvl + key_deleted(
+                        key, generate_diff(
+                            file_before[key],
+                            file_before[key],
+                            nesting_lvl=nesting_lvl + 1
+                        )
+                    )
+                    result += "    " * nesting_lvl + key_added(
+                        key, file_after[key]
+                    )
+                elif not isinstance(
+                    file_before.get(key), dict
+                ) and isinstance(
+                    file_after[key], dict
+                ):
+                    result += "    " * nesting_lvl + key_deleted(
+                        key, file_before[key]
+                    )
+                    result += "    " * nesting_lvl + key_added(
+                        key, generate_diff(
+                            file_after[key],
+                            file_after[key],
+                            nesting_lvl=nesting_lvl + 1
+                        )
+                    )
+                else:
+                    result += "    " * nesting_lvl + key_deleted(
+                        key, file_before[key]
+                    )
+                    result += "    " * nesting_lvl + key_added(
+                        key, file_after[key]
+                    )
+    result += "    " * nesting_lvl + "}"
     return edit_message(result)
 
 
-def check_values(key, file1, file2):
-    if file1[key] == file2[key]:
-        return f"    {key}: {file1[key]}\n"
-    else:
-        return f"  - {key}: {file1[key]}\n  + {key}: {file2[key]}\n"
+def key_deleted(key, value):
+    return f"  - {key}: {value}\n"
+
+
+def key_added(key, value):
+    return f"  + {key}: {value}\n"
+
+
+def key_not_changed(key, value):
+    return f"    {key}: {value}\n"
 
 
 def edit_message(message):
