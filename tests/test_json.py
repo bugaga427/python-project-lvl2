225051767807dee31f2d72eea7d59a02c0c43db5
bugaga427 <vladislav3372@gmail.com>
2021-03-23 12:52:11 +0300

    update tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -7,3 +7,10 @@ def test_json():
     after = json.load(open("tests/fixtures/json_after.json"))
     result = open("tests/fixtures/diff.txt")
     assert generate_diff(before, after) == result.read()
+
+
+def test_json_recursive():
+    before = json.load(open('tests/fixtures/json_recursive_before.json'))
+    after = json.load(open('tests/fixtures/json_recursive_after.json'))
+    result = open("tests/fixtures/diff_recursive.txt")
+    assert generate_diff(before, after) == result.read()
--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -13,3 +13,16 @@ def test_yaml():
     )
     result = open("tests/fixtures/diff.txt")
     assert generate_diff(before, after) == result.read()
+
+
+def test_yaml_recursive():
+    before = yaml.load(
+        open('tests/fixtures/yaml_recursive_before.yaml'),
+        Loader=yaml.FullLoader
+    )
+    after = yaml.load(
+        open('tests/fixtures/yaml_recursive_after.yaml'),
+        Loader=yaml.FullLoader
+    )
+    result = open("tests/fixtures/diff_recursive.txt")
+    assert generate_diff(before, after) == result.read()
