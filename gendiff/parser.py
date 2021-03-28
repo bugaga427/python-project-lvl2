2303dc902014c8cfea9cc3ca8dd8be14503e387c
bugaga427 <vladislav3372@gmail.com>
2021-03-29 02:29:36 +0300

    update parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,9 +1,17 @@
+from gendiff.formatters.stylish import render as stylish
 import argparse
+
+FORMATS = {
+    "stylish": stylish
+}
 
 
 def generate_parser():
     parser = argparse.ArgumentParser(description="Generate diff")
-    parser.add_argument("-f", "--format", help="set format of output")
+    parser.add_argument("-f", "--format",
+                        choices=FORMATS.keys(),
+                        default=stylish,
+                        help="set format of output")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     args = parser.parse_args()
