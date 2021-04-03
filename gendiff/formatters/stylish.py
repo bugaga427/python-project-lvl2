8c02b9775cc21bce443aa5c5bcf1a25d5e51ae30
bugaga427 <vladislav3372@gmail.com>
2021-04-04 02:26:58 +0300

    update stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,10 +1,32 @@
-def render(message, lvl=0):
-    result = ""
-    result += "{\n"
-    for key, value in message:
-        if isinstance(value, list):
-            result += "    " * lvl + f"{key} {render(value, lvl + 1)}"
-        else:
-            result += "    " * lvl + f"{key} {value}\n"
-    result += "    " * lvl + "}\n"
+def render(data):
+    result = {}
+    for item in data:
+        status, key = item[0], item[1]
+        value = unpack_values(item[-1])
+        if status == "added":
+            result[f"  + {key}"] = value
+        elif status == "removed":
+            result[f"  - {key}"] = value
+        elif status == "updated":
+            old_value = unpack_values(item[2])
+            result[f"  - {key}"] = old_value
+            result[f"  + {key}"] = value
+        elif status == "no changed":
+            result[f"    {key}"] = value
+    return result
+
+
+def unpack_values(value):
+    if isinstance(value, list):
+        return render(value)
+    return value
+
+
+def to_string(data, lvl=0):
+    result = "{\n"
+    for key, value in data.items():
+        if isinstance(value, dict):
+            value = to_string(value, lvl + 1)
+        result += f"{'    ' * lvl}{key}: {value}\n"
+    result += f"{'    ' * lvl}}}"
     return result
