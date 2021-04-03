4291f2f83a3178cd3c311deb4d10522fa2521a26
bugaga427 <vladislav3372@gmail.com>
2021-04-04 02:27:07 +0300

    update engine.py

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,18 +1,19 @@
 from gendiff.parsing import parsing_args
-from gendiff.parser import generate_parser, FORMATS
+from gendiff.cli import generate_parser, FORMATS
 
 
 def gendiff():
     parser = generate_parser()
     file_before, file_after = parsing_args()
-    diff = generate_diff(file_before, file_after)
-    if parser.format in FORMATS:
-        print(edit_message(FORMATS[parser.format](diff)))
-    else:
-        print("This output format is not supported.")
+    diff = generate_diff(file_before, file_after, FORMATS[parser.format].render)
+    print(edit_message(FORMATS[parser.format].to_string(diff)))
 
 
-def generate_diff(file_before, file_after):
+def generate_diff(file_before, file_after, format_name):
+    return format_name(difference(file_before, file_after))
+
+
+def difference(file_before, file_after):
     result = []
     keys = sorted(set(file_before) | set(file_after))
     for key in keys:
@@ -29,31 +30,30 @@ def check_keys(key, file1, file2):
         return check_values(key, file1, file2)
     elif key in file1:
         result = check_values(key, file1, file1)
-        result[0][0] = "  -" + result[0][0][3:]
+        result[0][0] = "removed"
         return result
     elif key in file2:
         result = check_values(key, file2, file2)
-        result[0][0] = "  +" + result[0][0][3:]
+        result[0][0] = "added"
         return result
 
 
 def check_values(key, file1, file2):
     if file1[key] == file2[key]:
         value = file1[key]
-        if is_dict(file1[key]):
-            value = generate_diff(file1[key], file2[key])
-        return [[f"    {key}:", value]]
+        if is_dict(value):
+            value = difference(file1[key], file2[key])
+        return [["no changed", key, value]]
     else:
         value1 = file1[key]
         value2 = file2[key]
         if is_dict(file1[key]) and is_dict(file2[key]):
-            return [[f"    {key}:", generate_diff(file1[key], file2[key])]]
+            return [["no changed", key, difference(file1[key])]]
         elif is_dict(file1[key]):
-            value1 = generate_diff(file1[key], file1[key])
+            value1 = difference(file1[key], file1[key])
         elif is_dict(file2[key]):
-            value2 = generate_diff(file2[key], file2[key])
-        return [[f"  - {key}:", value1],
-                [f"  + {key}:", value2]]
+            value2 = difference(file2[key], file2[key])
+        return [["updated", key, value1, value2]]
 
 
 def edit_message(message):
@@ -63,4 +63,4 @@ def edit_message(message):
         message = message.replace("True", "true")
     if "None" in message:
         message = message.replace("None", "null")
-    return message[:-1]
+    return message
