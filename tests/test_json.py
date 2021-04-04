dfc6ea7b9d036ed3eb1b6f7bc61a13ce68255580
bugaga427 <vladislav3372@gmail.com>
2021-04-04 20:01:03 +0300

    update tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,6 +1,5 @@
-from gendiff.engine import generate_diff
-from gendiff.formatters import stylish
-from gendiff.engine import edit_message
+from gendiff.engine import generate_diff, edit_message
+from gendiff.formatters import stylish, plain
 import json
 
 
@@ -20,3 +19,12 @@ def test_json_recursive():
     assert edit_message(
         stylish.to_string(
             generate_diff(before, after, stylish.render))) == result.read()
+
+
+def test_json_plain():
+    before = json.load(open('tests/fixtures/json_recursive_before.json'))
+    after = json.load(open('tests/fixtures/json_recursive_after.json'))
+    result = open("tests/fixtures/diff_plain.txt")
+    assert edit_message(
+        plain.to_string(
+            generate_diff(before, after, plain.render))) == result.read()
--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -1,6 +1,5 @@
-from gendiff.engine import generate_diff
-from gendiff.formatters import stylish
-from gendiff.engine import edit_message
+from gendiff.engine import generate_diff, edit_message
+from gendiff.formatters import stylish, plain
 import yaml
 
 
@@ -32,3 +31,18 @@ def test_yaml_recursive():
     assert edit_message(
         stylish.to_string(
             generate_diff(before, after, stylish.render))) == result.read()
+
+
+def test_yaml_plain():
+    before = yaml.load(
+        open('tests/fixtures/yaml_recursive_before.yaml'),
+        Loader=yaml.FullLoader
+    )
+    after = yaml.load(
+        open('tests/fixtures/yaml_recursive_after.yaml'),
+        Loader=yaml.FullLoader
+    )
+    result = open("tests/fixtures/diff_plain.txt")
+    assert edit_message(
+        plain.to_string(
+            generate_diff(before, after, plain.render))) == result.read()
