25c0fe935f696ce1b980f563a33fa3f2ec1a8e09
bugaga427 <vladislav3372@gmail.com>
2021-04-15 20:22:55 +0300

    rewritted engine.py

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -14,10 +14,11 @@ def get_diff(file_before, file_after, format_name):
 
 
 def generate_difference(file_before, file_after):
-    result = []
+    result = {}
     keys = sorted(set(file_before) | set(file_after))
     for key in keys:
-        result.extend(check_keys(key, file_before, file_after))
+        status, value = check_keys(key, file_before, file_after)
+        result[(status, key)] = value
     return result
 
 
@@ -25,31 +26,49 @@ def is_child(data):
     return isinstance(data, dict)
 
 
-def check_keys(key, file1, file2):
-    if key in file1 and key in file2:
-        return check_values(key, file1, file2)
-    elif key in file1:
-        result = check_values(key, file1, file1)
-        result[0][0] = "removed"
-    elif key in file2:
-        result = check_values(key, file2, file2)
-        result[0][0] = "added"
-    return result
+def check_keys(key, before, after):
+    if key in before and key in after:
+        return key_remained(before[key], after[key])
+    elif key in before:
+        return key_removed(before[key])
+    elif key in after:
+        return key_added(after[key])
 
 
-def check_values(key, file1, file2):
-    if file1[key] == file2[key]:
-        value = file1[key]
-        if is_child(value):
-            value = generate_difference(file1[key], file2[key])
-        return [["no changed", key, value]]
+def key_remained(before, after):
+    if before == after:
+        if is_child(before):
+            return ("no change",
+                    ["children", generate_difference(
+                        before, after)])
+        else:
+            return "no change", ["value", before]
     else:
-        value1 = file1[key]
-        value2 = file2[key]
-        if is_child(file1[key]) and is_child(file2[key]):
-            return [["no changed", key, generate_difference(value1, value2)]]
-        elif is_child(file1[key]):
-            value1 = generate_difference(file1[key], file1[key])
-        elif is_child(file2[key]):
-            value2 = generate_difference(file2[key], file2[key])
-        return [["updated", key, value1, value2]]
+        if is_child(before) and is_child(after):
+            return ("no change",
+                    ["children", generate_difference(
+                        before, after)])
+        elif is_child(before):
+            return ("changed",
+                    [["children", generate_difference(
+                        before, before)],
+                        ["value", after]])
+        elif is_child(after):
+            return ("changed", [["value", before],
+                    ["children", generate_difference(
+                        after, after)]])
+        else:
+            return ("changed",
+                    [["value", before], ["value", after]])
+
+
+def key_added(after):
+    if is_child(after):
+        return "added", ["children", generate_difference(after, after)]
+    return "added", ["value", after]
+
+
+def key_removed(before):
+    if is_child(before):
+        return "removed", ["children", generate_difference(before, before)]
+    return "removed", ["value", before]
