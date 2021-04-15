3f3e267e858409b8403e0452c0d3417a81095399
bugaga427 <vladislav3372@gmail.com>
2021-04-15 23:46:31 +0300

    fix engine.py

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -30,45 +30,39 @@ def check_keys(key, before, after):
     if key in before and key in after:
         return key_remained(before[key], after[key])
     elif key in before:
-        return key_removed(before[key])
+        return key_in_one_file(before[key], "removed")
     elif key in after:
-        return key_added(after[key])
+        return key_in_one_file(after[key], "added")
 
 
 def key_remained(before, after):
     if before == after:
+        status = "no change"
         if is_child(before):
-            return ("no change",
-                    ["children", generate_difference(
-                        before, after)])
+            value = ["children", generate_difference(before, after)]
         else:
-            return "no change", ["value", before]
+            value = ["value", before]
     else:
+        status = "changed"
         if is_child(before) and is_child(after):
-            return ("no change",
-                    ["children", generate_difference(
-                        before, after)])
+            status = "no change"
+            value = ["children", generate_difference(before, after)]
         elif is_child(before):
-            return ("changed",
-                    [["children", generate_difference(
-                        before, before)],
-                        ["value", after]])
+            value = [
+                ["children", generate_difference(before, before)],
+                ["value", after]
+            ]
         elif is_child(after):
-            return ("changed", [["value", before],
-                    ["children", generate_difference(
-                        after, after)]])
+            value = [
+                ["value", before],
+                ["children", generate_difference(after, after)]
+            ]
         else:
-            return ("changed",
-                    [["value", before], ["value", after]])
+            value = [["value", before], ["value", after]]
+    return status, value
 
 
-def key_added(after):
-    if is_child(after):
-        return "added", ["children", generate_difference(after, after)]
-    return "added", ["value", after]
-
-
-def key_removed(before):
-    if is_child(before):
-        return "removed", ["children", generate_difference(before, before)]
-    return "removed", ["value", before]
+def key_in_one_file(data, status):
+    if is_child(data):
+        return status, ["children", generate_difference(data, data)]
+    return status, ["value", data]
