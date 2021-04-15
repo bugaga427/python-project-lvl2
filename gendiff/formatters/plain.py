78a5b294ef478ac9f335100456d4e730cad9ab40
bugaga427 <vladislav3372@gmail.com>
2021-04-16 02:02:32 +0300

    fix plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -10,17 +10,18 @@ def format(data, key_path=""):
     for item in data.keys():
         status = item[0]
         key = key_path + item[1]
-        if status == "changed":
-            old_value = data[item][0]
-            new_value = data[item][1]
-            result.extend(key_changed(key, old_value, new_value))
-        elif status == "added":
-            value_type, value = data[item]
-            value = unpach_values(value, value_type)
-            result.extend([f"Property '{key}' was added with value: {value}"])
+        if status == "added":
+            value = unpach_values(data[item][1])
+            result.append(f"Property '{key}' was added with value: {value}")
         elif status == "removed":
-            result.extend([f"Property '{key}' was removed"])
-        elif status == "no change":
+            result.append(f"Property '{key}' was removed")
+        elif status == "changed":
+            old_value = unpach_values(data[item][0][1])
+            new_value = unpach_values(data[item][1][1])
+            result.append(
+                f"Property '{key}' was updated. From {old_value} to {new_value}"
+            )
+        else:
             value_type, value = data[item]
             if value_type == "children":
                 result.extend(format(value, key + "."))
@@ -30,11 +31,11 @@ def format(data, key_path=""):
 def key_changed(key, old_value, new_value):
     old_value = unpach_values(old_value[1], old_value[0])
     new_value = unpach_values(new_value[1], new_value[0])
-    return [f"Property '{key}' was updated. From {old_value} to {new_value}"]
+    return f"Property '{key}' was updated. From {old_value} to {new_value}"
 
 
-def unpach_values(value, value_type):
-    if value_type == "children":
+def unpach_values(value):
+    if isinstance(value, dict):
         return "[complex value]"
     elif isinstance(value, str):
         return f"'{value}'"
