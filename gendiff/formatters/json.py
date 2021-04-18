b1904a9ee72526e2e75a12a47149ef2d5d89b038
bugaga427 <vladislav3372@gmail.com>
2021-04-19 01:08:03 +0300

    fix json.py

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -12,14 +12,14 @@ def format(data):
     for item in data.keys():
         status, key = item
         if status == "changed":
-            old_value = check_value(data[item][0][1], data[item][0][0])
-            new_value = check_value(data[item][1][1], data[item][1][0])
+            old_value = check_value(data[item][0])
+            new_value = check_value(data[item][1])
             value = ["updated", old_value, new_value]
         else:
             if status == "no change":
-                value = check_value(data[item][1], data[item][0])
+                value = check_value(data[item])
             else:
-                value = [status, check_value(data[item][1], data[item][0])]
+                value = [status, check_value(data[item])]
         result[key] = value
     return result
 
@@ -28,7 +28,8 @@ def is_child(value_type):
     return value_type == "children"
 
 
-def check_value(value, value_type):
+def check_value(value):
+    value_type, value = value
     if is_child(value_type):
         value = format(value)
     return value
