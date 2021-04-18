b6951a5a941eafb370bd4374f038b87ecf8920da
bugaga427 <vladislav3372@gmail.com>
2021-04-19 00:59:24 +0300

    fix json.py

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -2,27 +2,33 @@ import json
 
 
 def render(data):
-    return json.dumps(format(data), indent=4)
+    data = format(data)
+    data = json.dumps(data, indent=4)
+    return data
 
 
 def format(data):
     result = {}
-    for item in data:
-        status, key = item[0], item[1]
-        value = unpack_values(item[-1])
-        if status == "added":
-            result[key] = [status, value]
-        elif status == "removed":
-            result[key] = [status, value]
-        elif status == "updated":
-            old_value = unpack_values(item[2])
-            result[key] = [status, old_value, value]
-        elif status == "no changed":
-            result[key] = value
+    for item in data.keys():
+        status, key = item
+        if status == "changed":
+            old_value = check_value(data[item][0][1], data[item][0][0])
+            new_value = check_value(data[item][1][1], data[item][1][0])
+            value = ["updated", old_value, new_value]
+        else:
+            if status == "no change":
+                value = check_value(data[item][1], data[item][0])
+            else:
+                value = [status, check_value(data[item][1], data[item][0])]
+        result[key] = value
     return result
 
 
-def unpack_values(value):
-    if isinstance(value, list):
-        return format(value)
+def is_child(value_type):
+    return value_type == "children"
+
+
+def check_value(value, value_type):
+    if is_child(value_type):
+        value = format(value)
     return value
